dc9b8c4027ab8718bb95664022e1fea50ef044bc
IliyaKobaliya <kobaliya0506@gmail.com>
2018-10-30 19:32:49 +0200

    DIST (for start need 1)'npm install -g serve'; 2)'serve -s build')

---
+++ precache-manifest.68c7153ab481a979266ac3fb4d4682d6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ecb38ff0895e3c72bef7285733602b3",
+    "url": "/static/media/calendar.8ecb38ff.png"
+  },
+  {
+    "revision": "88bacfff555479ac59ff",
+    "url": "/static/css/main.ef3f6587.chunk.css"
+  },
+  {
+    "revision": "845dd15cf4fd39384fb0",
+    "url": "/static/js/1.845dd15c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f5ee47a48dabc9cac1e1cbd0ca477cbc",
+    "url": "/static/media/GreenArrow.f5ee47a4.png"
+  },
+  {
+    "revision": "d24b79301f85009b92d23fed0d8ebd84",
+    "url": "/static/media/document.d24b7930.png"
+  },
+  {
+    "revision": "866e363e12bf0296abe384367acc8e4b",
+    "url": "/static/media/play.866e363e.png"
+  },
+  {
+    "revision": "88bacfff555479ac59ff",
+    "url": "/static/js/main.88bacfff.chunk.js"
+  },
+  {
+    "revision": "d2affff08cb64d59de515080390008e2",
+    "url": "/static/media/LogoPhone.d2affff0.jpg"
+  },
+  {
+    "revision": "6bdd53a7b16ffdd095428b3e408f885e",
+    "url": "/static/media/avatar.6bdd53a7.jpeg"
+  },
+  {
+    "revision": "50f97829d6e7803af5e88baa5b900039",
+    "url": "/static/media/Download.50f97829.png"
+  },
+  {
+    "revision": "d0e3a1ba9c7f0b918901a1c56ab88509",
+    "url": "/static/media/SmartPhone.d0e3a1ba.png"
+  },
+  {
+    "revision": "127081ee28130c2e3cb0f559741ba03b",
+    "url": "/static/media/RedArrow.127081ee.png"
+  },
+  {
+    "revision": "3018bcaceaa1a50b2796a912ac91bd4d",
+    "url": "/static/media/downLo.3018bcac.png"
+  },
+  {
+    "revision": "a4dc40519e521a83cd666849ed31018d",
+    "url": "/index.html"
+  }
+];
